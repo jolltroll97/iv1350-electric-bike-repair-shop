a466ea84d74339b468c49835b32923acc15cf4ec
Joel Lagerberg <joel.hansson.lagerberg@kth.se>
2026-04-22 16:40:25 +0200

    DTOs: constructors, getters and toString method added

--- src/se/kth/iv1350/repairshop/dto/RepairTaskDTO.java
+++ src/se/kth/iv1350/repairshop/dto/RepairTaskDTO.java
@@ -1,5 +1,57 @@
 package se.kth.iv1350.repairshop.dto;
-
+/**
+ * Contains information about one specific repair task
+ */
 public class RepairTaskDTO {
     
+    private final String description;
+    private final int cost;
+    private final int time;
+
+    /**
+     * Creates a new instance representing a specific repair task
+     * 
+     * @param description   Description of the repair task.
+     * @param cost          The price of the repair task.
+     * @param time          The estimated time it takes for the technician
+     *                      to complete the repair task.
+     */
+
+    public RepairTaskDTO(String description, int cost, int time){
+        this.description = description;
+        this.cost = cost;
+        this.time = time;
+    }
+
+    /**
+     * Getters for all attributes.
+     */
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    /**
+     * Method for printing the DTOs attributes.
+     */
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Description: ").append(description).append("\n")
+               .append("Cost: ").append(cost).append("\n")
+               .append("Time: ").append(time);
+               
+        return builder.toString();
+    }
+
 }
