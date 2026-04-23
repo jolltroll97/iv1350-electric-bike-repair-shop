3cdb35e080d148bfb5f4ddbca122960af9fa0393
jolltroll97 <joel.hansson.lagerberg@gmail.com>
2026-04-23 19:32:48 +0200

    Update RegistryCreator.java

--- src/se/kth/iv1350/repairshop/integration/RegistryCreator.java
+++ src/se/kth/iv1350/repairshop/integration/RegistryCreator.java
@@ -1,5 +1,29 @@
 package se.kth.iv1350.repairshop.integration;
 
 public class RegistryCreator {
+    private final CustomerRegistry customerRegistry;
+    private final RepairOrderRegistry repairOrderRegistry;
+
+
+    /**
+     * Creates new instances of the registries available in the database.
+     */
+    public RegistryCreator(){
+        this.customerRegistry = new CustomerRegistry();
+        this.repairOrderRegistry = new RepairOrderRegistry();
+    }
+
+    /**
+     * Gets the registry containing all customer data.
+     */
+    public CustomerRegistry getCustomerRegistry(){
+        return this.customerRegistry;
+    }
     
+    /**
+     * Gets the registry containing the data for all repair orders.
+     */
+    public RepairOrderRegistry getRepairOrderRegistry(){
+        return this.repairOrderRegistry;
+    }
 }
