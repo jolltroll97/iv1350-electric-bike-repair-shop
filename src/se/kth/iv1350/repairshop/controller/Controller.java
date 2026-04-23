40af86962fd0a29e31f7cfdeabb11e0cdc746238
jolltroll97 <joel.hansson.lagerberg@gmail.com>
2026-04-23 19:15:55 +0200

    Update Controller.java

--- src/se/kth/iv1350/repairshop/controller/Controller.java
+++ src/se/kth/iv1350/repairshop/controller/Controller.java
@@ -1,10 +1,128 @@
 package se.kth.iv1350.repairshop.controller;
 
+import se.kth.iv1350.repairshop.integration.RegistryCreator;
+import se.kth.iv1350.repairshop.integration.CustomerRegistry;
+import se.kth.iv1350.repairshop.integration.RepairOrderRegistry;
+
+import se.kth.iv1350.repairshop.dto.CustomerDTO;
+import se.kth.iv1350.repairshop.dto.DiagnosticReportDTO;
+import se.kth.iv1350.repairshop.dto.RepairOrderDTO;
+import se.kth.iv1350.repairshop.dto.RepairTaskDTO;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Controller {
 
-    public CustomerDTO (int phoneNum){
-        
-        
+    private final CustomerRegistry customerRegistry;
+    private final RepairOrderRegistry repairOrderRegistry;
+
+    private int currentRepairOrder;
+
+    /**
+     * Creates a new instance of the Controller.
+     * 
+     * @param creator       Used to get the classes that handle the database calls.
+     */
+
+    public Controller(RegistryCreator creator){
+        this.customerRegistry = creator.getCustomerRegistry();
+        this.repairOrderRegistry = creator.getRepairOrderRegistry();
+    }
+
+    /**
+     * Retrieves customer info by searching for their phone number.
+     * 
+     * @param phoneNum      The customers phone number.
+     */
+    public CustomerDTO retrieveCustomerInfo(int phoneNum){
+        /**
+         * No code yet.
+         */
+    }
+
+    /**
+     * Creates a new repair order.
+     * 
+     * @param customer      The customer connected to the repair order.
+     * @param date          The date rthe repair order was created.
+     * @param repairReport  Description of the repair report (receptionist enters this).
+     */
+    public void createRepairOrder(CustomerDTO customer, int date, String repairReport){
+        /**
+         * No code yet.
+         */
+    }
+
+    /**
+     * Returns a list of repair orders from the registry, filtered by state.
+     * 
+     * @param state         The state that filters the returned list.
+     */
+    public List<RepairOrderDTO> retrieveRepairOrderList(String state){
+        /**
+         * Det borde vara en lista som returneras, eller hur?
+         * 
+         * No code yet.
+         */
+    }
+
+    /**
+     * Adds a diagnostic report to a specific repair order.
+     * NOTE: does NOT save the update to the registry.
+     * 
+     * @param repairTask    The specific task to be added.
+     * @param repairOrderId The ID number used to identify the correct repair order in the registry.
+     */
+    public DiagnosticReportDTO addDiagnosticReport(RepairTaskDTO repairTask, int repairOrderId){
+        /**
+         * No code yet.
+         */
     }
     
+    /**
+     * Updates the repair order by adding a diagnostic report.
+     * This is done after each repair task that is to be added.
+     * 
+     * @param reportDTO     The diagnostic report to be added to the repair order.
+     * @param repairOrderId The ID of the repair report that is to be updated.              
+     */
+    public void updateRepairOrder(DiagnosticReportDTO reportDTO, int repairOrderId){
+        /**
+         * No code yet.
+         */
+    }
+
+    /**
+     * Returns a list of all repair orders connected to a specific customer.
+     * 
+     * @param phoneNum      The phone number belonging to the specific customer.
+     */
+    public List<RepairOrderDTO> getByPhoneNum(int phoneNum){
+        /**
+         * Det borde vara en lista som returneras, eller hur?
+         * 
+         * Registret borde använda:
+         * return Collections.unmodifiableList(dtoList);
+         * Detta gör att view inte råkar ta bort något ur listan.
+         * 
+         * No code yet.
+         * Förslag på användning:
+         * List<RepairOrderDTO> dtoList = new ArrayList<>();
+         */
+    }
+
+    /**
+     * Updates the repair order with the customers response.
+     * Can be "Accepted" or "Rejected"
+     * 
+     * @param response              The customers response.
+     * @param selectedRepairOrder   The repair order that is to be updated
+     *                              (chosen from the list returned from "getByPhoneNum")
+     */
+    public void customerResponse(boolean response, RepairOrderDTO selectedRepairOrder){
+        /**
+         * No code yet.
+         */
+    }
 }
