2cd368f464261f94ac10566486ef4c4dd49766a1
Isak Ahlberg <iahlberg@kth.se>
2026-04-24 14:20:36 +0200

    DiagnosticReport
    DiagnosticReport, adds repairtask to repairtaskslist and return the updated list. calculates the total time by summing time from all repair tasks with gettime from repairTaskDTO.

--- src/se/kth/iv1350/repairshop/model/DiagnosticReport.java
+++ src/se/kth/iv1350/repairshop/model/DiagnosticReport.java
@@ -1,14 +1,27 @@
 package se.kth.iv1350.repairshop.model;
+import se.kth.iv1350.repairshop.dto.DiagnosticReportDTO;
+import se.kth.iv1350.repairshop.dto.RepairTaskDTO;
+import se.kth.iv1350.repairshop.dto.RepairOrderDTO;
+import java.util.ArrayList;
+
 
 public class DiagnosticReport {
+    private ArrayList<RepairTaskDTO> repairTasksList;
+    
     public DiagnosticReport(DiagnosticReportDTO reportDTO) {
+        this.repairTasksList = new ArrayList<>();
     }
     
-    public int calculateTotalTime(RepairTaskDTO[] time) {
-        return 0;
+    public RepairTaskDTO[] addToList(RepairTaskDTO repairTask, RepairOrderDTO repairOrderDTO) {
+        repairTasksList.add(repairTask);
+        return repairTasksList.toArray(new RepairTaskDTO[0]);
     }
     
-    public RepairTaskDTO[] calculateAll(RepairOrderDTO repairOrderDTO) {
-        return null;
+    public int calculateTotalTime(RepairTaskDTO[] repairTasks) {
+        int totalTime = 0;
+        for (RepairTaskDTO task : repairTasks) {
+            totalTime += task.getTime();
+        }
+        return totalTime;
     }
 }
