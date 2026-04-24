738c773865fb726d8365aff7f1e1f9d433bb0ddf
Isak Ahlberg <iahlberg@kth.se>
2026-04-24 13:19:41 +0200

    OrderHandler done!
    Made it so accepted order gets created, printed and updated. Rejected order gets created and updated. Order gets updated with all neccesary info.

--- src/se/kth/iv1350/repairshop/model/OrderHandler.java
+++ src/se/kth/iv1350/repairshop/model/OrderHandler.java
@@ -1,13 +1,40 @@
 package se.kth.iv1350.repairshop.model;
+import se.kth.iv1350.repairshop.dto.RepairOrderDTO;
+import se.kth.iv1350.repairshop.integration.RepairOrderRegistry;
+import se.kth.iv1350.repairshop.integration.Printer;
+
 
 public class OrderHandler {
-    public void orderRejected(RepairOrderDTO selectedRepairOrder) {
-        /*Logik */
-        /*retunerar inget */
+    private Printer printer;
+    private RepairOrderRegistry repairOrderRegistry;
+
+    public OrderHandler(Printer printer, RepairOrderRegistry repairOrderRegistry) {
+        this.printer = printer;
+        this.repairOrderRegistry = repairOrderRegistry;
     }
 
-    public void orderAccepted(RepairOrderDTO selectedRepairOrder){
-        /*Logik */
-        /*retunerar inget */
-    } 
+    public void orderAccepted(RepairOrderDTO selectedRepairOrder) {
+        // Update order state to "Accepted" and persist
+        RepairOrderDTO acceptedOrder = createUpdatedOrder(selectedRepairOrder, "Accepted");
+        printer.printRepairOrder(acceptedOrder);
+        repairOrderRegistry.updateRepairOrder(acceptedOrder);
+    }
+
+    public void orderRejected(RepairOrderDTO selectedRepairOrder) {
+        // Update order state to "Rejected" and persist
+        RepairOrderDTO rejectedOrder = createUpdatedOrder(selectedRepairOrder, "Rejected");
+        repairOrderRegistry.updateRepairOrder(rejectedOrder);
+    }
+
+    private RepairOrderDTO createUpdatedOrder(RepairOrderDTO order, String newState) {
+        return new RepairOrderDTO(
+            order.getReportDTO(),
+            order.getDate(),
+            order.getTotalCost(),
+            order.getRepairReport(),
+            newState,  // Update the state
+            order.getCustomer(),
+            order.getRepairId()
+        );
+    }
 }
