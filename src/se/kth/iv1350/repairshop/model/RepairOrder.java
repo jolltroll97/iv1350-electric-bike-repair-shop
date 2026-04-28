b0f7ebb059d3ecf5f7b741ef9bea1bc31f558167
jolltroll97 <joel.hansson.lagerberg@gmail.com>
2026-04-28 15:47:51 +0200

    RepairOrderTest

--- src/se/kth/iv1350/repairshop/model/RepairOrder.java
+++ src/se/kth/iv1350/repairshop/model/RepairOrder.java
@@ -1,11 +1,13 @@
 package se.kth.iv1350.repairshop.model;
 
 import se.kth.iv1350.repairshop.dto.RepairTaskDTO;
+import java.util.List;
+import java.util.ArrayList;
 
 public class RepairOrder {
-    public int calculateTotal(RepairTaskDTO[] cost) {
+    public int calculateTotal(List<RepairTaskDTO> tasks) {
         int totalCost = 0;
-        for (RepairTaskDTO task : cost) {
+        for (RepairTaskDTO task : tasks) {
             totalCost += task.getCost();
         }
         return totalCost;
