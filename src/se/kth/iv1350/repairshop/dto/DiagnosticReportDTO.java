b28a68bf31cae4562038f25500368439813e6306
Isak Ahlberg <iahlberg@kth.se>
2026-04-28 16:09:08 +0200

    Javadoc comment for the code
    javadoc comment made for DiagnosticReportDTO, RepairOrderRegistry, DiagnosticReport and RepairOrder.

--- src/se/kth/iv1350/repairshop/dto/DiagnosticReportDTO.java
+++ src/se/kth/iv1350/repairshop/dto/DiagnosticReportDTO.java
@@ -2,30 +2,60 @@ package se.kth.iv1350.repairshop.dto;
 
 import java.util.ArrayList;
 
+/**
+ * This object has information about repair tasks
+ * and the total time required to complete them. It is used to transfer
+ * diagnostic report data between different layers of the application.
+ */
 public class DiagnosticReportDTO {
     // Attributes
     private ArrayList<RepairTaskDTO> repairTasksList;
     private int totalTime;
     
+    /**
+     * Creates a new DiagnosticReportDTO with the specified repair tasks and total time.
+     * 
+     * @param repairTasksList The list of repair tasks included in this diagnostic report.
+     * @param totalTime The total time required to complete all repair tasks, typically measured in minutes.
+     */
     public DiagnosticReportDTO(ArrayList<RepairTaskDTO> repairTasksList, int totalTime) {
         this.repairTasksList = repairTasksList;
         this.totalTime = totalTime;
     }
     
-    // Getters
+    /**
+     * Retrieves the list of repair tasks in this diagnostic report.
+     * 
+     * @return An ArrayList containing all repair tasks.
+     */
     public ArrayList<RepairTaskDTO> getRepairTasksList() {
         return repairTasksList;
     }
     
+    /**
+     * Retrieves the total time required to complete all repair tasks.
+     * 
+     * @return The total time in minutes.
+     */
     public int getTotalTime() {
         return totalTime;
     }
     
-    // Setters
+    /**
+     * Updates the list of repair tasks in this diagnostic report.
+     * 
+     * @param repairTasksList The new list of repair tasks to set.
+     */
     public void setRepairTasksList(ArrayList<RepairTaskDTO> repairTasksList) {
         this.repairTasksList = repairTasksList;
     }
     
+    /**
+     * Returns a string representation of this diagnostic report.
+     * The string includes the number of repair tasks and the total time required.
+     * 
+     * @return A formatted string describing the diagnostic report
+     */
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Repair Tasks: ").append(repairTasksList.size())
--- src/se/kth/iv1350/repairshop/model/RepairOrder.java
+++ src/se/kth/iv1350/repairshop/model/RepairOrder.java
@@ -2,10 +2,23 @@ package se.kth.iv1350.repairshop.model;
 
 import se.kth.iv1350.repairshop.dto.RepairTaskDTO;
 
+/**
+ * Represents a repair order in the repair shop system.
+ * Manages the calculation and handling of repair tasks and their associated costs.
+ */
 public class RepairOrder {
+    
+    /**
+     * Calculates the total cost of all repair tasks in the order.
+     * 
+     * @param cost An array of RepairTaskDTO objects representing the tasks to be performed.
+     * @return The total cost as an integer, representing the sum of all task costs.
+     */
     public int calculateTotal(RepairTaskDTO[] cost) {
         int totalCost = 0;
+        // Iterate through each repair task
         for (RepairTaskDTO task : cost) {
+            // Add the cost of the current task to the running total
             totalCost += task.getCost();
         }
         return totalCost;
